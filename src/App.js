bc7a714579454f6aaa827bee8e02ebc66e6c1008
Rahil Raj <98189273+rahilraj1@users.noreply.github.com>
2023-02-26 16:52:43 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Restraunt from './Restraunt';
+import Home from './Home';
+function App() 
+{
+    return (
+        <div>
+            <Home></Home>
+            <Restraunt />
+        </div>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,17 @@
+const ok=()=>
+{
+    alert("Here I am");
+}
+function Home()
+{
+    return(
+        <div>
+            <html>
+                <form>
+                    
+                </form>
+            </html>
+        </div>
+    )
+}
+export default Home;
